1a5f666d2be30719ccd2683efde4e2de9cd69692
IURII <yralei@gmail.com>
2021-10-28 16:20:40 +0300

    commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['eslint-config-airbnb-base', 'prettier', 'plugin:react/recommended'],
+  parser: '@babel/eslint-parser',
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'react/prop-types': 0,
+    'import/prefer-default-export': 0,
+    'no-console': 0,
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -4,52 +4,50 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production';
-    const config = {
-        entry: './src/index.jsx',
-        output: {
-            filename: 'bundle.js',
+  const isProduction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.jsx',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /.jsx?$/,
+          use: ['babel-loader'],
         },
-        module: {
-            rules: [
-                {
-                    test: /.jsx?$/,
-                    use: ['babel-loader'],
-                },
-                {
-                    test: /.s?css$/,
-                    use: [
-                        isProduction
-                            ? MiniCssExtractPlugin.loader
-                            : 'style-loader',
-                        'css-loader',
-                        'sass-loader',
-                    ],
-                },
-            ],
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
         },
-        resolve: {
-            extensions: ['.js', '.jsx'],
-        },
-        plugins: [
-            new webpack.ProgressPlugin(),
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: './src/index.html',
-            }),
-        ],
-        devServer: {
-            hot: true,
-        },
-    };
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+    devServer: {
+      hot: true,
+    },
+  };
 
-    if (isProduction) {
-        config.plugins.push(
-            new MiniCssExtractPlugin({
-                filename: '[name].css',
-            }),
-        );
-    }
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      })
+    );
+  }
 
-    return config;
+  return config;
 };
