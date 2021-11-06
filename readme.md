56865356322d70bc938dcab1ac4b7558638836a3
IURII <yralei@gmail.com>
2021-11-06 19:15:06 +0200

    commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# calendar-project
+
+## Quick start
+
+[click this link](https://youthful-northcutt-0ba5a4.netlify.app/)
+
+### **_Author: Iurii Babitskyi_**
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { months } from '../../utils/dateUtils.js';
-
 import './header.scss';
 
 const Header = ({ onLeft, onRight, month, onToday, onEventCreate }) => {
